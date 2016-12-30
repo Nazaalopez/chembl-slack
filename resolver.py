3f9363e6d82158a4a8ceeccbe3b13e5a2af01de8
Michał Nowotka <mmmnow@gmail.com>
2016-12-30 20:47:40 +0000

    Update resolver.py

--- resolver.py
+++ resolver.py
@@ -1,6 +1,39 @@
 import re
 from chembl_webresource_client.utils import utils
 from chembl_webresource_client.unichem import unichem_client as unichem
+from chembl_webresource_client.unichem import UniChemClient
+
+class CorrectedUniChemClient(UniChemClient):
+    def get(self, pk, src_id=None, to_src_id=None, all=False, url=False, verbose=False):
+      if pk.upper().startswith('CHEMBL'):
+          if to_src_id:
+              if url:
+                  url = '{0}/src_compound_id_url/{1}/{2}/{3}'.format(self.base_url, pk, src_id, to_src_id)
+              elif all:
+                  url = '{0}/src_compound_id_all/{1}/{2}/{3}'.format(self.base_url, pk, src_id, to_src_id)
+              else:
+                  url = '{0}/src_compound_id/{1}/{2}/{3}'.format(self.base_url, pk, src_id, to_src_id)
+          else:
+              if all:
+                  url = '{0}/src_compound_id_all/{1}/{2}'.format(self.base_url, pk, src_id)
+              else:
+                  url = '{0}/src_compound_id/{1}/{2}'.format(self.base_url, pk, src_id)
+      elif inchi_key_regex.match(pk):
+          if all:
+              url = '{0}/inchikey_all/{1}'.format(self.base_url, pk)
+          elif verbose:
+              url = '{0}/verbose_inchikey/{1}'.format(self.base_url, pk)
+          else:
+              url = '{0}/inchikey/{1}'.format(self.base_url, pk)
+      else:
+          if to_src_id:
+              url = '{0}/src_compound_id_all_obsolete/{1}/{2}/{3}'.format(self.base_url, pk, src_id, to_src_id)
+          else:
+              if src_id:
+                  url = '{0}/src_compound_id_all_obsolete/{1}/{2}'.format(self.base_url, pk, src_id)
+              else:
+                  url = '{0}/orphanIdMap/{1}'.format(self.base_url, pk) 
+      return self._get_results(url)
 
 inchi_key_regex = re.compile('[A-Z]{14}-[A-Z]{10}-[A-Z]')
 smilesRegex = re.compile(r'^([^J][.0-9BCGOHMNSEPRIFTLUA@+\-\[\]\(\)\\\/%=#$]+)$')
