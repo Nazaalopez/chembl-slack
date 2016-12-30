bd0ef193bc3e3d5d3a83687d84995a5311fb38c0
Michał Nowotka <mmmnow@gmail.com>
2016-12-30 20:13:40 +0000

    Update resolver.py

--- resolver.py
+++ resolver.py
@@ -1,1 +1,12 @@
+import re
+from chembl_webresource_client.utils import utils
 
+inchi_key_regex = re.compile('[A-Z]{14}-[A-Z]{10}-[A-Z]')
+smilesRegex = re.compile(r'^([^J][.0-9BCGOHMNSEPRIFTLUA@+\-\[\]\(\)\\\/%=#$]+)$')
+
+def resolve(mystery):
+  if smilesRegex.match(mystery.upper()):
+    return utils.inchi2inchiKey(utils.ctab2inchi(utils.smiles2ctab(mystery)))
+  elif mystery.upper().startswith('INCHI='):
+    return utils.inchi2inchiKey(mystery)
+  return False
