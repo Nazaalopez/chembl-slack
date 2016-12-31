845a7c1a4a479575fca423b906977632ba48987c
Michał Nowotka <mmmnow@gmail.com>
2016-12-31 12:20:00 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@
 
 from sys import argv
 import json
-import time
 import re
 
 from bottle import Bottle
@@ -19,12 +18,7 @@ molecule.set_format('json')
 TOKEN = 'dWwqGfNcPy2gcwZu41zc2BuN'
 inchi_key_regex = re.compile('[A-Z]{14}-[A-Z]{10}-[A-Z]')
 
-#from chembl_webresource_client.settings import Settings
-#Settings.Instance().TOTAL_RETRIES = 1
-#Settings.Instance().TIMEOUT = 0.5
-#Settings.Instance().NEW_CLIENT_TIMEOUT = 0.5
-
-from compound_template import MESSAGE_TEMPLATE
+from compound_view import render_compound
 
 @app.post('/chem')
 def chem():
@@ -45,19 +39,8 @@ def chem():
             elif ret.get(1):
                 reply = molecule.get(ret.get(1))
         if reply:        
-            msg = MESSAGE_TEMPLATE.copy()
+            msg = render_compound(reply)
             chembl_id = reply["molecule_chembl_id"]
-            msg["attachments"][0]["author_name"] = reply["pref_name"]
-            msg["attachments"][0]["title"] = chembl_id
-            msg["attachments"][0]["title_link"] = "https://chembl-glados.herokuapp.com/compound_report_card/{0}/".format(chembl_id)
-            msg["attachments"][0]["image_url"] = "https://www.ebi.ac.uk/chembl/api/data/image/{0}.png?engine=indigo&ignoreCoords=1&dimensions=500".format(chembl_id)
-            msg["attachments"][0]["thumb_url"] = "https://www.ebi.ac.uk/chembl/api/data/image/{0}.png?engine=indigo&ignoreCoords=1&dimensions=50".format(chembl_id)
-            msg["attachments"][0]["text"] = reply["molecule_structures"]["standard_inchi_key"]
-            msg["attachments"][0]["fields"][0]["value"] = reply["max_phase"]
-            msg["attachments"][0]["fields"][1]["value"] = reply["molecule_properties"]["full_molformula"]
-            msg["attachments"][0]["fields"][2]["value"] = reply["molecule_structures"]["canonical_smiles"]
-            msg["attachments"][0]["fields"][3]["value"] = reply["molecule_structures"]["standard_inchi"]
-            msg["attachments"][0]["ts"] = int(time.time())
             response.content_type = 'application/json'
             return json.dumps(msg)
         else:
