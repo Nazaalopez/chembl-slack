91e3648ac961d62502a437e3abec0a7ed5810f14
Michał Nowotka <mmmnow@gmail.com>
2016-12-31 12:34:09 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@
 
 from sys import argv
 import json
-import re
 
 from bottle import Bottle
 from bottle import debug, request, route, response, post
@@ -16,7 +15,6 @@ molecule = new_client.molecule
 molecule.set_format('json')
 
 TOKEN = 'dWwqGfNcPy2gcwZu41zc2BuN'
-inchi_key_regex = re.compile('[A-Z]{14}-[A-Z]{10}-[A-Z]')
 
 from compound_view import render_compound
 
@@ -25,26 +23,14 @@ def chem():
     # Check the token and make sure the request is from our team
     reply = None
     if hasattr(request, 'forms') and request.forms['token'] == TOKEN:
-        text = request.forms['text']
-        if text.startswith('CHEMBL') or inchi_key_regex.match(text):
-            reply = molecule.get(text)   
-        else:    
-            ret = resolve(text)
-            if not ret:
-                ret = molecule.search(text)
-                if not len(ret):
-                    return "Provided identifier couldn't be resolved :white_frowning_face:"
-                else:
-                    reply = ret[0]
-            elif ret.get(1):
-                reply = molecule.get(ret.get(1))
-        if reply:        
-            msg = render_compound(reply)
-            chembl_id = reply["molecule_chembl_id"]
-            response.content_type = 'application/json'
-            return json.dumps(msg)
-        else:
-            return "This compound wasn't found in ChEMBL but it can be found in following databases:"
+        text = request.forms['text']  
+        ret = resolve(text)
+        if not ret:
+            return "Provided identifier couldn't be resolved :white_frowning_face:"      
+        msg = render_compound(ret)
+        chembl_id = reply["molecule_chembl_id"]
+        response.content_type = 'application/json'
+        return json.dumps(msg)
 
 debug(True)
 app.run(host='0.0.0.0', port=argv[1])
