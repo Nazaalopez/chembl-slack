956c45dc854797e2aa328e8fb6155824724f11a3
Michał Nowotka <mmmnow@gmail.com>
2017-01-04 16:28:14 +0000

    Update serialize.py

--- chembl_slack/plugins/serialize.py
+++ chembl_slack/plugins/serialize.py
@@ -2,14 +2,18 @@ from bottle import request, response
 from chembl_slack import config
 import json
 
-def authorize(callback):
-    def wrapper(*args, **kwargs):
-        body = callback(*args, **kwargs)
-        if isinstance(body, basestring):
-            return body
-        elif isinstance(body, dict)::
-            response.content_type = 'application/json'
-            body["response_type"] = config.get('response_type', 'ephemeral')
-            return json.dumps(body)
-        return None    
-return wrapper
+class Serialize(object):
+    name = 'serialize'
+    api = 2
+
+    def apply(self, fn, context):
+        def _serialize(*args, **kwargs):
+            body = return fn(*args, **kwargs)
+            if isinstance(body, basestring):
+                return body
+            elif isinstance(body, dict):
+                response.content_type = 'application/json'
+                body["response_type"] = config.get('response_type', 'ephemeral')
+                return json.dumps(body)
+            return None               
+        return _serialize
