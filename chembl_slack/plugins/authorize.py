25afe7b412243fd9df712fe018a2d330e3cfa74a
Michał Nowotka <mmmnow@gmail.com>
2017-01-04 16:26:00 +0000

    Update authorize.py

--- chembl_slack/plugins/authorize.py
+++ chembl_slack/plugins/authorize.py
@@ -1,11 +1,14 @@
 from bottle import request, response
 from chembl_slack import config
 
-def authorize(callback):
-    def wrapper(*args, **kwargs):
-        if hasattr(request, 'forms') and 'token' in request.forms and request.forms['token'] == config.get('token'):
-            body = callback(*args, **kwargs)
-            return body
-        response.status = 401
-        return "Not authorized"
-    return wrapper
+class Authorize(object):
+    name = 'authorize'
+    api = 2
+
+    def apply(self, fn, context):
+        def _authorize(*args, **kwargs):
+            if hasattr(request, 'forms') and 'token' in request.forms and request.forms['token'] == config.get('token'):
+                return fn(*args, **kwargs)
+            response.status = 401
+                return "Not authorized"
+        return _authorize
